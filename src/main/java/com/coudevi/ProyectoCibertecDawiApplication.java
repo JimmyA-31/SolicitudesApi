d4bb35cd48762a3640018a665f6eef3ddc7262b4
Percy Valverde <mastrohos3@gmail.com>
2025-04-18 11:53:10 -0500

    Primer commit

---
+++ src/main/java/com/coudevi/ProyectoCibertecDawiApplication.java
@@ -0,0 +1,13 @@
+package com.coudevi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoCibertecDawiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoCibertecDawiApplication.class, args);
+	}
+
+}
